e33b46ddc14cb31ab044afcb57f4a4df2abc54ab
Fábio Santos <fabio.santos@fundacaoabc.org>
2020-07-23 21:46:54 -0300

    initial commit configured all project

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+import routes from './routes';
+
+const app = express();
+
+app.use(routes);
+
+app.listen(7000, () => {
+  console.log(' Server started on port 7000');
+});
